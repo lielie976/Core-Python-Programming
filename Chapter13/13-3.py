23939ddc23fa506e55f6f303af3c3980b0f74267
fengfeng <fengfeng@qq.co>
2016-05-22 12:53:00 +0800

    .

--- Chapter13/13-3.py
+++ Chapter13/13-3.py
@@ -1,26 +1,52 @@
 
-def dollarize(amount):
-    val = str(amount)
-    isNegtive = False
-    if val.startswith("-"):
-        isNegtive = True
-        val = val[1:]
-    Money = val.split(".")
-    num = len(Money[0]) / 3
-    mod = len(Money[0]) % 3
-    if mod == 0:
-        num -= 1
-    strTemp = []
-    for i in range(num):
-        strTemp.append(Money[0][-3:])
-        Money[0] = Money[0][:-3]
-    strTemp.append(Money[0])
-    strTemp.reverse()
-    myDoller = ','.join(strTemp) + "." + Money[1]
-    if isNegtive:
-        myDoller = "-" + myDoller
-    return myDoller
+class MoneyFmt(object):
 
-if __name__ == "__main__":
-    doller = dollarize(-11123434.3244)
-    print doller
+    def __init__(self, amount, normal=True):
+        if isinstance(amount, float):
+            self.amount = amount
+            self.value = ""
+            self.normal = normal
+        else:
+            raise TypeError("Input a float")
+
+    def update(self):
+        self.dollarize()
+
+    def dollarize(self):
+        val = str(self.amount)
+        isNegtive = False
+        if val.startswith("-"):
+            isNegtive = True
+            val = val[1:]
+        Money = val.split(".")
+        num = len(Money[0]) / 3
+        mod = len(Money[0]) % 3
+        if mod == 0:
+            num -= 1
+        strTemp = []
+        for i in range(num):
+            strTemp.append(Money[0][-3:])
+            Money[0] = Money[0][:-3]
+        strTemp.append(Money[0])
+        strTemp.reverse()
+        myDoller = ','.join(strTemp) + "." + Money[1]
+        if isNegtive:
+            if self.normal:
+                myDoller = "-" + myDoller
+            else:
+                myDoller = "<->" + myDoller
+        self.value = myDoller
+
+    def __nonzero__(self):
+        strTemp = self.value.split(".")
+        if strTemp in ("0", "-0"):
+            return False
+        else:
+            return True
+
+    def __repr__(self):
+        return str(self.amount)
+
+    def __str__(self):
+        return self.value
+
